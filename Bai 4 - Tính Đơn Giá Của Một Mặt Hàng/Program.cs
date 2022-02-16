23bc654a49f3ef5b71ec3f7000991656c42b6f89
Keios Starqua <khoi0941@gmail.com>
2022-02-16 15:35:11 +0700

    Tạo bài 4

---
+++ Bai 4 - Tính Đơn Giá Của Một Mặt Hàng/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bai_4___Tính_Đơn_Giá_Của_Một_Mặt_Hàng
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
