33cf53b43f3b7d919e15f069653c77d42776d7de
Keios Starqua <khoi0941@gmail.com>
2022-02-16 15:42:53 +0700

    Xong Bai 4

--- Bai 4 - Tính Đơn Giá Của Một Mặt Hàng/Program.cs
+++ Bai 4 - Tính Đơn Giá Của Một Mặt Hàng/Program.cs
@@ -6,7 +6,27 @@ namespace Bai_4___Tính_Đơn_Giá_Của_Một_Mặt_Hàng
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double donGia;
+            int soLuong;
+            double thanhTien = 0;
+
+            // Nhập Đơn Giá
+            Console.Write("Nhập Đơn giá: ");
+            donGia = double.Parse(Console.ReadLine());
+
+            // Nhập Số Lượng
+            Console.Write("Nhập Số lượng: ");
+            soLuong = int.Parse(Console.ReadLine());
+
+            // Thành Tiền trước khi giảm giá
+            thanhTien = donGia * soLuong;
+
+            // Xét điều kiện để giảm giá
+            if (thanhTien > 100)
+            {
+                thanhTien = thanhTien - thanhTien * 0.03;
+            }
+            Console.Write("Thành tiền là: " + thanhTien);
         }
     }
 }
