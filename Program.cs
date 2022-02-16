5cdf2775854e870b8fedaadac243eb7aad63ed2f
Keios Starqua <khoi0941@gmail.com>
2022-02-16 13:52:54 +0700

    VIết xong chương trình kiểm tra số nguyên nhập vào  là âm hay dương

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace Bai_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Cho phép in ra Tiếng Việt
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            int iSoN; // Tạo biến số nguyên n
+            // Yêu cầu nhập n
+            Console.Write("Nhập vào số nguyên n : ");
+            iSoN = int.Parse(Console.ReadLine()); // Parse để ép kiểu
+
+            //sử dụng cấu trúc điều kiện if
+            if (iSoN == 0)
+            {
+                Console.Write("n là Số không âm không dương");
+            }
+            if (iSoN > 0)
+            {
+                Console.Write("Kết quả: n là Số dương");
+            }
+            if (iSoN < 0)
+            {
+                Console.Write("Kết quả: n là Số âm");
+            }
         }
     }
 }
