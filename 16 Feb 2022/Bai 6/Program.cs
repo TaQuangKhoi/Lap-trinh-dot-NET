182412f5453f312843114cbbc354a321b5292881
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2022-02-23 11:41:35 +0700

    fix: Bai 6: Logic

--- 16 Feb 2022/Bai 6/Program.cs
+++ 16 Feb 2022/Bai 6/Program.cs
@@ -10,9 +10,11 @@ namespace Bai_6
             for (int i = 1; i <= 9; i++)
             {
                 for (int j = 1; j <=10; j++)
-            {
-            Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
+                {
+                    Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
+                }
             }
+            Console.WriteLine();
         }
     }
 }
