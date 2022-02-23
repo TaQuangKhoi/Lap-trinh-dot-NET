a306315f34d40ade2b9473c66900bdbe8066f5a4
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2022-02-23 10:57:40 +0700

    Xong Bai 5

--- 16 Feb 2022/Bai 5/Program.cs
+++ 16 Feb 2022/Bai 5/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace Bai_5
+namespace Bai_6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int soDienTieuThu; double thanhTien = 0;
+            Console.Write("Moi nhap so luong dien tieu thu: ");
+            soDienTieuThu = int.Parse(Console.ReadLine());
+            if (soDienTieuThu <= 100)
+            {
+            thanhTien += soDienTieuThu * 5;
+            }
+            else if (soDienTieuThu <=150)
+            {
+            thanhTien += 100 * 5 + (soDienTieuThu- 100) * 7;
+            }
+            else if (soDienTieuThu <=200)
+            {
+            thanhTien += 100 * 5 + 50 * 7+ (soDienTieuThu-150) * 10;
+            }
+            else if (soDienTieuThu <= 300)
+            {
+            thanhTien += 100 * 5 + 50 * 7 + 50 * 10 + (soDienTieuThu-200) * 15;
+            }
+            else
+            {
+            //Nếu vượt kế hoạch (300 Kwh) thực hiện như sau:
+            thanhTien += 100 * 5 + 50 * 7 + 50 * 10 + 100 * 15 +
+            (soDienTieuThu- 300) * 20;
+            }
+            Console.WriteLine("So tien ban phai tra la: " + thanhTien);
         }
     }
 }
