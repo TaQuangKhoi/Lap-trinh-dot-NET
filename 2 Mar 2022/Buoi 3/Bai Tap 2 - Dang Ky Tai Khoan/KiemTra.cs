7ef70ff5504eadf1a1e83bece04ac94aea6d1775
Keios Starqua <khoi0941@gmail.com>
2022-03-03 21:49:47 +0700

    Thêm domain outlook

--- 2 Mar 2022/Buoi 3/Bai Tap 2 - Dang Ky Tai Khoan/KiemTra.cs
+++ 2 Mar 2022/Buoi 3/Bai Tap 2 - Dang Ky Tai Khoan/KiemTra.cs
@@ -21,11 +21,12 @@ namespace Bai_Tap_2___Dang_Ky_Tai_Khoan
                 return false;
             }
             //Tìm vị trí @ trong chuỗi email
-            int index1 = email.IndexOf("@"); int index2 = email.IndexOf(".com"); //Lấy ra tên miền nếu có của email
+            int index1 = email.IndexOf("@");
+            int index2 = email.IndexOf(".com"); //Lấy ra tên miền nếu có của email
             string domain = email.Substring(index1 + 1, index2 - index1 - 1);
             //Tại đây bạn có thể thêm miền nào muốn vào đây
             //Nếu không nằm trong đây trả về false 
-            if (domain != "gmail" && domain != "hotmail")
+            if (domain != "gmail" && domain != "hotmail" && domain != "outlook")
             {
                 return false;
             }
