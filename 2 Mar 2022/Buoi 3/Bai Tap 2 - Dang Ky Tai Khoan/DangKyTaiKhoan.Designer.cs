d00b0f700c29446a3f1d7a5e86209fa9ee973b8f
Keios Starqua <khoi0941@gmail.com>
2022-03-03 21:33:50 +0700

    Thêm 3 label (*)
    Đổi tên button1 thành buttonDangKy

--- 2 Mar 2022/Buoi 3/Bai Tap 2 - Dang Ky Tai Khoan/DangKyTaiKhoan.Designer.cs
+++ 2 Mar 2022/Buoi 3/Bai Tap 2 - Dang Ky Tai Khoan/DangKyTaiKhoan.Designer.cs
@@ -37,7 +37,10 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonDangKy = new System.Windows.Forms.Button();
+            this.labelStar1 = new System.Windows.Forms.Label();
+            this.labelStar2 = new System.Windows.Forms.Label();
+            this.labelStar3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblDangKyTaiKhoan
@@ -48,7 +51,7 @@
             this.lblDangKyTaiKhoan.ForeColor = System.Drawing.Color.Black;
             this.lblDangKyTaiKhoan.Location = new System.Drawing.Point(0, 0);
             this.lblDangKyTaiKhoan.Name = "lblDangKyTaiKhoan";
-            this.lblDangKyTaiKhoan.Size = new System.Drawing.Size(500, 100);
+            this.lblDangKyTaiKhoan.Size = new System.Drawing.Size(485, 100);
             this.lblDangKyTaiKhoan.TabIndex = 0;
             this.lblDangKyTaiKhoan.Text = "Đăng Ký Tài Khoản";
             this.lblDangKyTaiKhoan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -121,23 +124,56 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Xác Nhận Mật Khẩu";
             // 
-            // button1
+            // buttonDangKy
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(180, 350);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 50);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Đăng Ký";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonDangKy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDangKy.Location = new System.Drawing.Point(180, 350);
+            this.buttonDangKy.Name = "buttonDangKy";
+            this.buttonDangKy.Size = new System.Drawing.Size(100, 50);
+            this.buttonDangKy.TabIndex = 9;
+            this.buttonDangKy.Text = "Đăng Ký";
+            this.buttonDangKy.UseVisualStyleBackColor = true;
+            this.buttonDangKy.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelStar1
+            // 
+            this.labelStar1.AutoSize = true;
+            this.labelStar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStar1.Location = new System.Drawing.Point(429, 126);
+            this.labelStar1.Name = "labelStar1";
+            this.labelStar1.Size = new System.Drawing.Size(25, 20);
+            this.labelStar1.TabIndex = 10;
+            this.labelStar1.Text = "(*)";
+            // 
+            // labelStar2
+            // 
+            this.labelStar2.AutoSize = true;
+            this.labelStar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStar2.Location = new System.Drawing.Point(429, 177);
+            this.labelStar2.Name = "labelStar2";
+            this.labelStar2.Size = new System.Drawing.Size(25, 20);
+            this.labelStar2.TabIndex = 11;
+            this.labelStar2.Text = "(*)";
+            // 
+            // labelStar3
+            // 
+            this.labelStar3.AutoSize = true;
+            this.labelStar3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStar3.Location = new System.Drawing.Point(429, 227);
+            this.labelStar3.Name = "labelStar3";
+            this.labelStar3.Size = new System.Drawing.Size(25, 20);
+            this.labelStar3.TabIndex = 12;
+            this.labelStar3.Text = "(*)";
             // 
             // DangKyTaiKhoan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelStar3);
+            this.Controls.Add(this.labelStar2);
+            this.Controls.Add(this.labelStar1);
+            this.Controls.Add(this.buttonDangKy);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -166,7 +202,10 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonDangKy;
+        private System.Windows.Forms.Label labelStar1;
+        private System.Windows.Forms.Label labelStar2;
+        private System.Windows.Forms.Label labelStar3;
     }
 }
 
