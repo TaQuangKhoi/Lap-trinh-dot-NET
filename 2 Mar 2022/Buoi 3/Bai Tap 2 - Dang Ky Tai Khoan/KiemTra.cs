1b7d69d02debe733d27f00bda951ced18c243acd
Keios Starqua <khoi0941@gmail.com>
2022-03-03 21:47:48 +0700

    Copy hàm kiểm tra email từ file pdf qua

--- 2 Mar 2022/Buoi 3/Bai Tap 2 - Dang Ky Tai Khoan/KiemTra.cs
+++ 2 Mar 2022/Buoi 3/Bai Tap 2 - Dang Ky Tai Khoan/KiemTra.cs
@@ -8,5 +8,28 @@ namespace Bai_Tap_2___Dang_Ky_Tai_Khoan
 {
     internal class KiemTra
     {
+        public bool Email (string email)
+        {
+            //Kiểm tra có chứa @ không? 
+            if (!email.Contains("@"))
+            {
+                return false;
+            }
+            //Nếu không chứa ".com" -> Sai 
+            if (!email.Contains(".com"))
+            {
+                return false;
+            }
+            //Tìm vị trí @ trong chuỗi email
+            int index1 = email.IndexOf("@"); int index2 = email.IndexOf(".com"); //Lấy ra tên miền nếu có của email
+            string domain = email.Substring(index1 + 1, index2 - index1 - 1);
+            //Tại đây bạn có thể thêm miền nào muốn vào đây
+            //Nếu không nằm trong đây trả về false 
+            if (domain != "gmail" && domain != "hotmail")
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
