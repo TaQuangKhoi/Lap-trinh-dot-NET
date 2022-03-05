67f3ee81d1c8d624a176f25e48745b930c81f882
Keios Starqua <khoi0941@gmail.com>
2022-03-05 19:41:32 +0700

    Copy from Exercise 3
    Sao chép từ bải tập 3

--- 2 Mar 2022 - Buoi 3/Bai Tap 4_Tinh Tong Day So/Form1.Designer.cs
+++ 2 Mar 2022 - Buoi 3/Bai Tap 4_Tinh Tong Day So/Form1.Designer.cs
@@ -28,20 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonThoat = new System.Windows.Forms.Button();
+            this.buttonTiepTuc = new System.Windows.Forms.Button();
+            this.buttonThucHien = new System.Windows.Forms.Button();
+            this.txtBxboiSo = new System.Windows.Forms.TextBox();
+            this.lblBoiSo = new System.Windows.Forms.Label();
+            this.txtBxUocSo = new System.Windows.Forms.TextBox();
+            this.txtBxNhapSoB = new System.Windows.Forms.TextBox();
+            this.txtBxNhapSoA = new System.Windows.Forms.TextBox();
+            this.lblUocSo = new System.Windows.Forms.Label();
+            this.lblNhapB = new System.Windows.Forms.Label();
+            this.lblNhapA = new System.Windows.Forms.Label();
+            this.lblUocSoChung_BoiSoChung = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // buttonThoat
+            // 
+            this.buttonThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThoat.Location = new System.Drawing.Point(323, 388);
+            this.buttonThoat.Name = "buttonThoat";
+            this.buttonThoat.Size = new System.Drawing.Size(120, 50);
+            this.buttonThoat.TabIndex = 36;
+            this.buttonThoat.Text = "Thoát";
+            this.buttonThoat.UseVisualStyleBackColor = true;
+            // 
+            // buttonTiepTuc
+            // 
+            this.buttonTiepTuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTiepTuc.Location = new System.Drawing.Point(183, 388);
+            this.buttonTiepTuc.Name = "buttonTiepTuc";
+            this.buttonTiepTuc.Size = new System.Drawing.Size(120, 50);
+            this.buttonTiepTuc.TabIndex = 35;
+            this.buttonTiepTuc.Text = "Tiếp Tục";
+            this.buttonTiepTuc.UseVisualStyleBackColor = true;
+            // 
+            // buttonThucHien
+            // 
+            this.buttonThucHien.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThucHien.Location = new System.Drawing.Point(44, 388);
+            this.buttonThucHien.Name = "buttonThucHien";
+            this.buttonThucHien.Size = new System.Drawing.Size(120, 50);
+            this.buttonThucHien.TabIndex = 34;
+            this.buttonThucHien.Text = "Thực Hiện";
+            this.buttonThucHien.UseVisualStyleBackColor = true;
+            // 
+            // txtBxboiSo
+            // 
+            this.txtBxboiSo.Location = new System.Drawing.Point(243, 319);
+            this.txtBxboiSo.Name = "txtBxboiSo";
+            this.txtBxboiSo.ReadOnly = true;
+            this.txtBxboiSo.Size = new System.Drawing.Size(200, 20);
+            this.txtBxboiSo.TabIndex = 33;
+            // 
+            // lblBoiSo
+            // 
+            this.lblBoiSo.AutoSize = true;
+            this.lblBoiSo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoiSo.Location = new System.Drawing.Point(41, 323);
+            this.lblBoiSo.Name = "lblBoiSo";
+            this.lblBoiSo.Size = new System.Drawing.Size(158, 16);
+            this.lblBoiSo.TabIndex = 32;
+            this.lblBoiSo.Text = "Bội số chung nhỏ nhất";
+            // 
+            // txtBxUocSo
+            // 
+            this.txtBxUocSo.Location = new System.Drawing.Point(243, 269);
+            this.txtBxUocSo.Name = "txtBxUocSo";
+            this.txtBxUocSo.ReadOnly = true;
+            this.txtBxUocSo.Size = new System.Drawing.Size(200, 20);
+            this.txtBxUocSo.TabIndex = 31;
+            // 
+            // txtBxNhapSoB
+            // 
+            this.txtBxNhapSoB.Location = new System.Drawing.Point(168, 219);
+            this.txtBxNhapSoB.Name = "txtBxNhapSoB";
+            this.txtBxNhapSoB.Size = new System.Drawing.Size(275, 20);
+            this.txtBxNhapSoB.TabIndex = 30;
+            // 
+            // txtBxNhapSoA
+            // 
+            this.txtBxNhapSoA.Location = new System.Drawing.Point(168, 169);
+            this.txtBxNhapSoA.Name = "txtBxNhapSoA";
+            this.txtBxNhapSoA.Size = new System.Drawing.Size(275, 20);
+            this.txtBxNhapSoA.TabIndex = 29;
+            // 
+            // lblUocSo
+            // 
+            this.lblUocSo.AutoSize = true;
+            this.lblUocSo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUocSo.Location = new System.Drawing.Point(41, 273);
+            this.lblUocSo.Name = "lblUocSo";
+            this.lblUocSo.Size = new System.Drawing.Size(159, 16);
+            this.lblUocSo.TabIndex = 28;
+            this.lblUocSo.Text = "Ước số chung lớn nhất";
+            // 
+            // lblNhapB
+            // 
+            this.lblNhapB.AutoSize = true;
+            this.lblNhapB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNhapB.Location = new System.Drawing.Point(41, 223);
+            this.lblNhapB.Name = "lblNhapB";
+            this.lblNhapB.Size = new System.Drawing.Size(78, 16);
+            this.lblNhapB.TabIndex = 27;
+            this.lblNhapB.Text = "Nhập số b";
+            // 
+            // lblNhapA
+            // 
+            this.lblNhapA.AutoSize = true;
+            this.lblNhapA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNhapA.Location = new System.Drawing.Point(41, 173);
+            this.lblNhapA.Name = "lblNhapA";
+            this.lblNhapA.Size = new System.Drawing.Size(78, 16);
+            this.lblNhapA.TabIndex = 26;
+            this.lblNhapA.Text = "Nhập số a";
+            // 
+            // lblUocSoChung_BoiSoChung
+            // 
+            this.lblUocSoChung_BoiSoChung.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblUocSoChung_BoiSoChung.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblUocSoChung_BoiSoChung.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUocSoChung_BoiSoChung.ForeColor = System.Drawing.Color.Black;
+            this.lblUocSoChung_BoiSoChung.Location = new System.Drawing.Point(0, 0);
+            this.lblUocSoChung_BoiSoChung.Name = "lblUocSoChung_BoiSoChung";
+            this.lblUocSoChung_BoiSoChung.Size = new System.Drawing.Size(485, 100);
+            this.lblUocSoChung_BoiSoChung.TabIndex = 25;
+            this.lblUocSoChung_BoiSoChung.Text = "Ước Số Chung - Bội Số Chung";
+            this.lblUocSoChung_BoiSoChung.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.buttonThoat);
+            this.Controls.Add(this.buttonTiepTuc);
+            this.Controls.Add(this.buttonThucHien);
+            this.Controls.Add(this.txtBxboiSo);
+            this.Controls.Add(this.lblBoiSo);
+            this.Controls.Add(this.txtBxUocSo);
+            this.Controls.Add(this.txtBxNhapSoB);
+            this.Controls.Add(this.txtBxNhapSoA);
+            this.Controls.Add(this.lblUocSo);
+            this.Controls.Add(this.lblNhapB);
+            this.Controls.Add(this.lblNhapA);
+            this.Controls.Add(this.lblUocSoChung_BoiSoChung);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonThoat;
+        private System.Windows.Forms.Button buttonTiepTuc;
+        private System.Windows.Forms.Button buttonThucHien;
+        private System.Windows.Forms.TextBox txtBxboiSo;
+        private System.Windows.Forms.Label lblBoiSo;
+        private System.Windows.Forms.TextBox txtBxUocSo;
+        private System.Windows.Forms.TextBox txtBxNhapSoB;
+        private System.Windows.Forms.TextBox txtBxNhapSoA;
+        private System.Windows.Forms.Label lblUocSo;
+        private System.Windows.Forms.Label lblNhapB;
+        private System.Windows.Forms.Label lblNhapA;
+        private System.Windows.Forms.Label lblUocSoChung_BoiSoChung;
     }
 }
 
