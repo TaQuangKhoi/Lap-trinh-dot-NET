0f6d62ab481b7ccac709cf8b0adc5b46ee5400ed
Keios Starqua <khoi0941@gmail.com>
2022-03-23 22:12:06 +0700

    Đổi tên các thư mục

---
+++ 16 Feb 2022 - Buoi 1/Bai 6/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Bai_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bang cuu chuong 9 theo chieu doc co dang:\n");
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <=10; j++)
+                {
+                    Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
