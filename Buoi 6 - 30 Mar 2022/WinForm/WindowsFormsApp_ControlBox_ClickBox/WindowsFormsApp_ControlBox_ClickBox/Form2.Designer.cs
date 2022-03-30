18d022c946b26f181bfd874bfded69b5dcd67052
Khoi at BVU - 2PM01 <khoi0941@gmail.com>
2022-03-30 14:34:56 +0700

    điều kiện để hiện điểm chuẩn

--- Buoi 6 - 30 Mar 2022/WinForm/WindowsFormsApp_ControlBox_ClickBox/WindowsFormsApp_ControlBox_ClickBox/Form2.Designer.cs
+++ Buoi 6 - 30 Mar 2022/WinForm/WindowsFormsApp_ControlBox_ClickBox/WindowsFormsApp_ControlBox_ClickBox/Form2.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.cBxChonTruong = new System.Windows.Forms.ComboBox();
+            this.lblDiemChuan = new System.Windows.Forms.Label();
+            this.tBxDiemChuan = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // cBxChonTruong
@@ -36,23 +38,46 @@
             this.cBxChonTruong.FormattingEnabled = true;
             this.cBxChonTruong.Location = new System.Drawing.Point(101, 50);
             this.cBxChonTruong.Name = "cBxChonTruong";
-            this.cBxChonTruong.Size = new System.Drawing.Size(121, 21);
+            this.cBxChonTruong.Size = new System.Drawing.Size(200, 21);
             this.cBxChonTruong.TabIndex = 0;
+            this.cBxChonTruong.SelectedIndexChanged += new System.EventHandler(this.cBxChonTruong_SelectedIndexChanged);
+            // 
+            // lblDiemChuan
+            // 
+            this.lblDiemChuan.AutoSize = true;
+            this.lblDiemChuan.Location = new System.Drawing.Point(378, 57);
+            this.lblDiemChuan.Name = "lblDiemChuan";
+            this.lblDiemChuan.Size = new System.Drawing.Size(65, 13);
+            this.lblDiemChuan.TabIndex = 1;
+            this.lblDiemChuan.Text = "Điểm Chuẩn";
+            // 
+            // tBxDiemChuan
+            // 
+            this.tBxDiemChuan.Location = new System.Drawing.Point(450, 50);
+            this.tBxDiemChuan.Name = "tBxDiemChuan";
+            this.tBxDiemChuan.Size = new System.Drawing.Size(100, 20);
+            this.tBxDiemChuan.TabIndex = 2;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tBxDiemChuan);
+            this.Controls.Add(this.lblDiemChuan);
             this.Controls.Add(this.cBxChonTruong);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox cBxChonTruong;
+        private System.Windows.Forms.Label lblDiemChuan;
+        private System.Windows.Forms.TextBox tBxDiemChuan;
     }
 }
--- Buoi 6 - 30 Mar 2022/WinForm/WindowsFormsApp_ControlBox_ClickBox/WindowsFormsApp_ControlBox_ClickBox/Form2.cs
+++ Buoi 6 - 30 Mar 2022/WinForm/WindowsFormsApp_ControlBox_ClickBox/WindowsFormsApp_ControlBox_ClickBox/Form2.cs
@@ -16,5 +16,33 @@ namespace WindowsFormsApp_ControlBox_ClickBox
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            cBxChonTruong.Items.Add("Đại Học Bà Rịa Vũng Tàu");
+            cBxChonTruong.Items.Add("Đại Học Bách Khoa");
+            cBxChonTruong.Items.Add("Đại Học Khoa Hoc Tự Nhiên");
+            cBxChonTruong.Items.Add("Đại Học Luật");
+        }
+
+        private void cBxChonTruong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cBxChonTruong.SelectedIndex == 0)
+            {
+                tBxDiemChuan.Text = "18";
+            }
+            else if (cBxChonTruong.SelectedIndex == 1)
+            {
+                tBxDiemChuan.Text = "24";
+            }
+            else if (cBxChonTruong.SelectedIndex == 3)
+            {
+                tBxDiemChuan.Text = "20";
+            }
+            else if (cBxChonTruong.SelectedIndex == 4)
+            {
+                tBxDiemChuan.Text = "19";
+            }
+        }
     }
 }
