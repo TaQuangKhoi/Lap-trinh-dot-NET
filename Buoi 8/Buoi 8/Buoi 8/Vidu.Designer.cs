7867aa2c467c38e9c89e3755c7966820a082a5eb
Keios Starqua <khoi0941@gmail.com>
2022-04-27 16:34:00 +0700

    Xong nút sửa xóa

--- Buoi 8/Buoi 8/Buoi 8/Vidu.Designer.cs
+++ Buoi 8/Buoi 8/Buoi 8/Vidu.Designer.cs
@@ -47,9 +47,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(45, 119);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Khoa";
+            this.label1.Text = "Tên Khoa";
             // 
             // cbbKhoa
             // 
@@ -64,16 +64,16 @@
             // 
             this.dgvLop.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dgvLop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvLop.Location = new System.Drawing.Point(45, 206);
+            this.dgvLop.Location = new System.Drawing.Point(45, 225);
             this.dgvLop.Name = "dgvLop";
-            this.dgvLop.Size = new System.Drawing.Size(437, 150);
+            this.dgvLop.Size = new System.Drawing.Size(437, 172);
             this.dgvLop.TabIndex = 2;
             this.dgvLop.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLop_CellClick);
             this.dgvLop.SelectionChanged += new System.EventHandler(this.dgvLop_SelectionChanged);
             // 
             // btnHien
             // 
-            this.btnHien.Location = new System.Drawing.Point(45, 162);
+            this.btnHien.Location = new System.Drawing.Point(45, 181);
             this.btnHien.Name = "btnHien";
             this.btnHien.Size = new System.Drawing.Size(75, 38);
             this.btnHien.TabIndex = 3;
@@ -121,6 +121,7 @@
             this.btnThem.TabIndex = 8;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // btnXoa
             // 
@@ -146,7 +147,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 388);
+            this.ClientSize = new System.Drawing.Size(538, 433);
             this.Controls.Add(this.btnSua);
             this.Controls.Add(this.btnXoa);
             this.Controls.Add(this.btnThem);
